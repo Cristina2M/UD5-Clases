b231399df1103fd9bb2b76aafb21d124290d9b74
Cristina2M <cmormar2509@g.educaand.es>
2025-01-08 13:01:24 +0000

    Act4

--- Relacion9/src/Act4/Act4.java
+++ Relacion9/src/Act4/Act4.java
@@ -2,6 +2,12 @@ package Act4;
 //4. Implementa la clase autobús según el siguiente modelo
 
 public class Act4 {
-    
-    
+    public static void main(String[] args) {
+        
+   
+    Autobus plaza1 = new Autobus();
+    plaza1.setNumeroPlazas(65);
+
+    System.out.println("El número de plazas del autobús es de " + plaza1.getNumeroPlazas());
+    }
 }
